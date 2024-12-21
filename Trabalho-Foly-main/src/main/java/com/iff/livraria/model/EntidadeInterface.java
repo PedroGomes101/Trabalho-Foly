0e63446ff7320565a9cdc2e060a189172102dfb8
Pedro Gomes <151851456+PedroGomes101@users.noreply.github.com>
2024-12-21 15:36:47 -0300

    Delete Trabalho-Foly-main directory

--- Trabalho-Foly-main/src/main/java/com/iff/livraria/model/EntidadeInterface.java
+++
@@ -1,5 +0,0 @@
-package com.iff.livraria.model;
-
-public interface EntidadeInterface {
-    public int getId();
-}
--- Trabalho-Foly-main/src/main/java/com/iff/livraria/model/dao/InterfaceDao.java
+++
@@ -1,9 +0,0 @@
-
-package com.iff.livraria.model.dao;
-
-
-import java.util.List;
-
-public interface InterfaceDao<T> {
-    public List<T> listar() throws Exception;
-}
